fe8032b0391b1d3d87561a93b35be7cbf65ff2a0
Nicolò Danzi <nicolodanzi@msn.com>
2015-03-17 11:14:59 +0100

    added sequenza massima

---
+++ Sequenza_massima/sequenza_massima.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+
+#define MAXN 10000
+
+using namespace std;
+
+int P[MAXN];
+
+int main(int argc, char** argv) {
+	int n;
+  int M = INT_MIN;
+  int max, j;
+ 
+  ifstream input;
+  ofstream output;
+  
+  input.open("input.txt");
+  assert(input.is_open());
+  output.open("output.txt", ofstream::trunc);
+
+  input >> n;
+
+  int i = 0;
+  for(i = 0; i < n; i++)
+    input >> P[i];
+
+  input.close();
+
+  
+  for(i = 0; i < n; i++) {
+    max = P[i];
+    if(max > M) M = max;
+    for(j = i; j < n; j++) {
+      max += P[j];
+      if(max > M) M = max;
+    }
+  }
+  
+  
+  output << M << endl;
+  
+
+	return 0;
+}
