308d2d32955deebb5749585a0fce2c834b98b339
id965kgk <katiamaschi@hotmail.it>
2015-05-07 21:33:26 +0200

    cento su centooooooooooooooooooo

--- Generali/generali_schifo.cpp
+++ Generali/generali_schifo.cpp
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <stack> 
 #include <set>
-#include <queue>
 #include <vector> 
 #include <fstream>
 #include <cstring>
@@ -43,7 +42,7 @@
 #define ALLOC(type, length) (type*) malloc(sizeof(type) * (length))
 #define REALLOC(who, type, length) who = (type*) realloc(who, sizeof(type) * (length))
 
-//#define DEBUG 
+#define DEBUG 
 
 
 //dimensioni del grafo
@@ -72,10 +71,7 @@ typedef struct vertex_struct {
 } vertex_t;
 
 typedef struct vertex_lite_struct {
-    int *adj_list;  //lista di adiacenza del vertice
-    int compagno;           // mio compagno nel matching
-    int dist;               // profondità
-    int out_deg;
+    vector<int> *adj_list;  //lista di adiacenza del vertice
 } vertex_lite_t;
 
 typedef vertex_t* graph_t;
@@ -87,20 +83,16 @@ typedef graph_t tree_t;
 int T = 0; //contatore del tempo di visita della DFS
 
 graph_t G;
-graph_lite_t bip;
 int *map;
 int consiglieri = 0;
 int counter = 0;
 stack<int> S;
-int found_CCS = 0;
 
-graph_lite_t create_bipartition(int *CCS, int n);
-int maximum_match(int *CCS, int n);
+graph_lite_t create_bipartition(vector<int> *CCS);
+void maximum_match(graph_lite_t bip);
 
 void print_tree() {
-    #ifdef DEBUG
-    cout << consiglieri << endl;
-    #endif
+    //cout << consiglieri << endl;
     output << consiglieri << endl;
     FOR_EACH_VERTEX {
         if(P(i) == -1) {
@@ -123,8 +115,6 @@ void print_tree() {
             #endif
             output << P(i)<< " " << VERTEX(i) << endl;
         }
-        free(G[i].adj_list);
-        free(G[i].inc_list);
     }
 }
 
@@ -171,9 +161,9 @@ int DFS_visit(int u) {
 
             LOWLINK(u) = min(LOWLINK(u), COUNTER(w));
 
-        } else if(COLOR(w) == BLACK && P(w) == -1) {
-            
-            consiglieri--;
+        } else if(COLOR(w) == BLACK) {
+            if(P(w) == -1)
+                consiglieri--;
             P(w) = u;
         }
 
@@ -187,8 +177,8 @@ int DFS_visit(int u) {
 
     }
 
-    int padre, c=0;
-    int *CCS;
+    int padre;
+    vector<int> *CCS = new vector<int>;
     //controllo le componenti connesse
     if(LOWLINK(u) == COUNTER(u)) {
 
@@ -202,13 +192,7 @@ int DFS_visit(int u) {
 
             w = S.top();
             S.pop();
-            if(c == 0)
-                CCS = ALLOC(int, 1);
-            else
-                REALLOC(CCS, int, c+1);
-
-
-            COLOR(w) = RED;
+            CCS->push_back(w);
 
             ONSTACK(w) = 0;
             #ifdef DEBUG
@@ -216,7 +200,7 @@ int DFS_visit(int u) {
             cout << "\t\t" << VERTEX(w) << " is out stack";
             #endif
 
-            if(c == 0)
+            if(CCS->size() == 1)
                 padre = P(w);
             if(P(w) != -1)
                 consiglieri++;
@@ -226,19 +210,10 @@ int DFS_visit(int u) {
             cout << " and consigliere" << endl;
             cout << endl;
             #endif
-            if(IN_DEG(w) == 1 && w != u) {
-                P(w) = -1;
-            } else {
-                CCS[c]=w;
-                c++;
-            }
             
-
         } while(w != u);
-
-        if(c == 1) {
+        if(CCS->size() == 1) {
             P(u) = padre;
-            COLOR(u) = BLACK;
             if(P(u) != -1)
                 consiglieri--;
             #ifdef DEBUG
@@ -247,25 +222,21 @@ int DFS_visit(int u) {
                 cout << "\t\this father was " << VERTEX(padre) << " " << padre << endl;
             #endif
         } else {
-            if(found_CCS == 0) {
-                found_CCS = 1;
-                bip = ALLOC(vertex_lite_t, V+1); //alloco il grafo bipartito
-            }
-            create_bipartition(CCS, c);
-            maximum_match(CCS, c);
-            
-            free(CCS);
+            graph_lite_t bip = create_bipartition(CCS);
+            maximum_match(bip);
+            free(bip);
         }
         
 
     }
 
+    delete CCS;
 
 
 
     //visita terminata
-    if(COLOR(u) != RED)
-        COLOR(u) = BLACK;
+
+    COLOR(u) = BLACK;
 
 
     END_VISIT(u) = T;
@@ -278,167 +249,109 @@ int DFS_visit(int u) {
     return -1;
 }
 
-bool BFS_max_match(int *CCS, int n){
-    #ifdef DEBUG
-    cout << "\t\t\tstart BFS" << endl;
-    #endif
-    queue<int> q;
-    for(int i; i < n; i++) {
-        int u = CCS[i];
-        if(bip[u].compagno == V) {
-            bip[u].dist = 0;
-            q.push(u);
-            #ifdef DEBUG
-            cout << "\t\t\tenqueue " << u << "dist = 0" << endl;
-            #endif
-        } else {
-            bip[u].dist = INT_MAX;
-            #ifdef DEBUG
-            cout << "\t\t\tdist of " << u << " is int max" << endl;
-            #endif
-        }
-        
-    }
-    bip[V].dist = INT_MAX;
-    while(!q.empty()) {
-        int u = q.front();
-        q.pop();
-        #ifdef DEBUG
-        cout << "\t\t\tdequeue " << u << endl;
-        cout << "\t\t\t\this dist is " << bip[u].dist << endl;
-        cout << "\t\t\t\tdist of NIL is " << bip[V].dist << endl;
-        #endif
-
-        if(bip[u].dist < bip[V].dist) {
-
-            for(int i = 0; i < bip[u].out_deg; i++) {
-                int v = bip[u].adj_list[i];
-                #ifdef DEBUG
-                cout << "\t\t\t\tdist of " << bip[v].compagno << " is " << bip[bip[v].compagno].dist << endl;
-                #endif
-                if(bip[bip[v].compagno].dist == INT_MAX) {
-                    bip[bip[v].compagno].dist = bip[u].dist + 1;
-                    q.push(bip[v].compagno);
-                    #ifdef DEBUG
-                    cout << "\t\t\t\tdist of " << bip[v].compagno << " is now " << bip[bip[v].compagno].dist << " and enqueued" << endl;
-                    #endif
-                }
-            }
-        }
-    }
-    return bip[V].dist != INT_MAX;
-}
-bool DFS_max_match(int u){
-    #ifdef DEBUG
-    cout << "\t\t\tstart DFS on " << u << endl;
-    #endif
-    if(u != V) {
-        for(int i = 0; i < bip[u].out_deg; i++) {
-            int v = bip[u].adj_list[i];
-            #ifdef DEBUG
-            cout << "\t\t\t\tdist of " << bip[v].compagno << " is " << bip[bip[v].compagno].dist << endl;
-            #endif
-            if(bip[bip[v].compagno].dist == bip[u].dist + 1) {
-                if(DFS_max_match(bip[v].compagno)) {
-                    #ifdef DEBUG
-                    cout << "\t\t\t\tfound a match between " << u << " and " << v << endl;
-                    #endif
-                    bip[u].compagno = v;
-                    bip[v].compagno = u;
-                    return true;
-                }
-            }
-
-        }
-        #ifdef DEBUG
-        cout << "\t\t\t\tNOT found a match for " << u << endl;
-        #endif
-        bip[u].dist = INT_MAX;
-        return false;
-    }
-    return true;
-    
-}
-
-int maximum_match(int *CCS, int n) {
+void maximum_match(graph_lite_t bip) {
 
     #ifdef DEBUG
     cout << "\t\t------------ calculating maximum match------------" << endl;
     #endif
 
-    int matching = 0;
-    while(BFS_max_match(CCS, n)) {
+    char *B = ALLOC(char, V+2);
+    for(int i = 0; i < V+2; i++)
+        B[i] = 0;
+
+    int s = V;
+    int t = V+1;
+    stack<int> Stack;
+    Stack.push(s);
+    while(!Stack.empty()) {
         #ifdef DEBUG
-        cout << "\t\tBFS returned true" << endl;
+        cout << "\t\t TOP = " << Stack.top() << endl;
         #endif
-        for(int i = 0; i < n; i++) {
-            int u = CCS[i];
+        while(!bip[Stack.top()].adj_list->empty()) {
+            int w = bip[Stack.top()].adj_list->back();
+            bip[Stack.top()].adj_list->pop_back();
+
             #ifdef DEBUG
-            cout << "\t\tevalueting " << u << " which is in CCS" << endl;
+            cout << "\t\t\t w = " << w << endl;
             #endif
-            if(bip[u].compagno == V) {
-                if(DFS_max_match(u)) {
-                    matching++;
+            if(B[w] == 0) {
+                Stack.push(w);
+                if(Stack.top() != t) {
+                    B[Stack.top()] = 1;
                     #ifdef DEBUG
-                    cout << "\t\tFound a match!!" << endl;
+                    cout << "\t\t\t push " << w << " on the stack and in B" << endl;
                     #endif
+                } else {
+                    Stack.pop();
+                    while(!Stack.empty() && Stack.top() != s) {
+                        int padre = Stack.top();
+                        Stack.pop();
+                        int figlio = Stack.top();
+                        Stack.pop();
+                        P(figlio) = padre;
+                        consiglieri--;
+                        #ifdef DEBUG
+                        cout << "\t\t\t set " << padre << " as father of " << figlio << endl;
+                        #endif
+                    }
                 }
             }
+
         }
+        if(Stack.top() != s)
+            delete bip[Stack.top()].adj_list;
+        Stack.pop();
     }
-    for(int i = 0; i < n; i++) {
-        if(bip[CCS[i]].compagno != V) {
-            P(CCS[i]) = bip[CCS[i]].compagno;
-            consiglieri--;
-            #ifdef DEBUG
-            cout << "\t\t " << bip[CCS[i]].compagno << " is father of " << CCS[i] << endl;
-            #endif
-        } 
-        else {
-            P(CCS[i]) = -1;
-        }
-    }
-    return matching;
+
+
 }
 
 //crea un grafo bipartito con L = CCS e R = possibili padri degli elementi della CCS
-graph_lite_t create_bipartition(int *CCS, int n) {
+graph_lite_t create_bipartition(vector<int> *CCS) {
 
     #ifdef DEBUG
     cout << "\t\t------------ creating bipartition------------" << endl;
     #endif
-    
+
+    graph_lite_t bip = ALLOC(vertex_lite_t, V+2); //alloco il grafo bipartito
+    int s = V; // sorgente che è diretta verso tutti gli elementi di L
+    int t = V+1; //destinazione alla quale sono diretti tutti gli elementi di R
     int w, u;  
     //inizializzo il nuovo grafo
-    for(int i = 0; i < V+1; i++) {
-        bip[i].dist = INT_MAX;
-        bip[i].compagno = V;
+    for(int i = 0; i < V+2; i++) {
+        bip[i].adj_list = new vector<int>;
     }
+    //inizializzo la sorgente
+    bip[s].adj_list = CCS;
 
     //scorro tutti gli elementi della CCS
-    for(int i = 0; i < n; i++) {
-        u = CCS[i];
-        
+    for(int i = 0; i < CCS->size(); i++) {
+        u = CCS->at(i);
+        COLOR(u) = RED;
+        #ifdef DEBUG
+        cout << "\t\t--- " << u << " is in CCS " << endl;
+        #endif
+        int found = 0;
         //scorro tutti gli elementi della lista di incidenza di u (tutti i suoi possibili padri)
         for(int j = 0; j < IN_DEG(u); j++) {
-            int found = 0;
+            found = 0;
             w = INC(u, j);
             #ifdef DEBUG
             cout << "\t\t\t--- " << w << " is in INC of " << u << endl;
             #endif
-
+            //cerco se il possibile padre di w fa parte della CCS
+            for(int k = 0; k < CCS->size(); k++) {
+                if(w == CCS->at(k))
+                    found = 1;
+            }
             //se non fa parte allora posso aggiungerlo all'insieme R
-            if(LOWLINK(u) != LOWLINK(w)) {
-
+            if(found == 0) {
                 //inserisco w come vicino di u (possibile padre)
-                if(bip[u].out_deg == 0) {
-                    bip[u].adj_list = ALLOC(int, 1);
-                } else {    // altrimenti aumento il suo spazio
-                    REALLOC(bip[u].adj_list, int, bip[u].out_deg +1);
-                }
-                
-                bip[u].adj_list[bip[u].out_deg]= w;
-                bip[u].out_deg++;
+                bip[u].adj_list->push_back(w);
+
+                //inserisco u come vicino di w (possbile figlio) e t come destinazione di w
+                bip[w].adj_list->push_back(u);
+                bip[w].adj_list->push_back(t);
 
                 #ifdef DEBUG
                 cout << "\t\t\t--- " << w << " is a possible father for " << u << endl;
@@ -459,6 +372,36 @@ void DFS() {
         }
     }
     return;
+}
+
+int partition( vertex_t* v, int l, int r) {
+   int pivot, i, j;
+   vertex_t t;
+   pivot = v[l].in_degree;
+   i = l; j = r+1;
+        
+   while( 1)
+   {
+    do ++i; while( v[i].in_degree <= pivot && i <= r );
+    do --j; while( v[j].in_degree > pivot );
+    if( i >= j ) break;
+    t = v[i]; v[i] = v[j]; v[j] = t;
+   }
+   t = v[l]; v[l] = v[j]; v[j] = t;
+   return j;
+}
+
+void quickSort( vertex_t* v, int l, int r)
+{
+   int j;
+
+   if( l < r ) 
+   {
+       j = partition( v, l, r);
+       quickSort( v, l, j-1);
+       quickSort( v, j+1, r);
+   }
+    
 }
 
 
@@ -539,7 +482,6 @@ int main() {
     DFS();
 
     print_tree();
-    free(G);
 
     return 0;
 }
